cdd2a4dd756c8cf4fa8391ad95323ae4fd7d8ef2
Raj Kumar Yadav <rajkumar08bgh@gmail.com>
2024-10-08 10:53:04 +0530

    1st Change

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar">
           <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Tech Hub</Link>
+          <Link className="navbar-brand" to="/TechHub">Tech Hub</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -50,6 +50,7 @@ function App() {
 
         {/* Wrap Route components inside Routes */}
         <Routes>
+          <Route path="/TechHub" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/documentation" element={<Documentation />} />
